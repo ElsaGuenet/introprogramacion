8f8c131e993c2b90d82658e84732cb4dbaa3e58b
ElsaGuenet <elsa.guenet@laposte.net>
2021-02-14 18:13:45 -0500

    taller 1

---
+++ talleres/taller 1.py
@@ -0,0 +1,10 @@
+numero1 = 46
+numero2 = 118
+sumar = numero1 + numero2
+print("el resultado de la sumar es",sumar)
+restar = numero1 - numero2
+print("el resultado de la restar es", restar)
+multiplicar = numero1 * numero2
+print("el resultado de la multiplicar es", multiplicar)
+dividir = numero1 / numero2
+print("el resultado de la dividir es", dividir)
