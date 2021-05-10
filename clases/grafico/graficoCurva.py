b895ffa0dcc682d1bd7534e89e5944fc18d3773b
ElsaGuenet <elsa.guenet@laposte.net>
2021-05-10 13:28:20 -0500

    grafico

--- clases/grafico/graficoCurva.py
+++ clases/grafico/graficoCurva.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 ecgData = pd.read_csv("ecg.csv", encoding="UTF-8", header=0, delimiter=";").to_dict()
 print(ecgData.keys())
 muestra = list(ecgData["muestra"].values())
 print(muestra)
 voltaje = list(ecgData["valor"].values())
 print(voltaje[-10:])
+plt.plot(muestra, voltaje)
+plt.show()
