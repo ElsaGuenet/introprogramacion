ca2a1db1c3c96f43b5a80d6233b11fe278d74202
ElsaGuenet <elsa.guenet@laposte.net>
2021-05-11 10:08:39 -0500

    exam

--- talleres/Exam 3bis.py
+++ talleres/Exam 3bis.py
@@ -1,8 +1,25 @@
-# diccionario = {} 
-# for i in range(0,5):
-# 	print("entrega el nombre de un snack: ")
-# 	entradaSnackFav = input()
-# 	print("entrega el percio de este snack: ")
-# 	entradaPrecios = input()
-# 	diccionario[entradaSnackFav] = float(entradaPrecios)
-# print (diccionario)
+import matplotlib.pyplot as plt 
+items = list()
+prix = list()
+for i in range(0,5):
+	print("entrega el nombre de un snack: ")
+	entradaSnackFav = input()
+	print("entrega el percio de este snack: ")
+	entradaPrecios = input()
+	prix.append(float(entradaPrecios))
+	items.append(entradaSnackFav)
+
+
+# entradaSnackFav = ["mars","tobleron", "lion", "maltesers", "milka"]
+# entradaPrecios = [2, 2.5, 1.6, 2.5, 1.9]
+plt.bar(items,prix, color = "r")
+plt.title("Precio de los cinco snacks favorito del usuario")
+plt.xlabel("Snacks favoritos del usuario")
+plt.ylabel("Precios en euro")
+plt.savefig("graficoBarasSnack.png")
+plt.show()
+	
+plt.bar(dic.keys(),dic.items(), color = "r")
+
+item.append(raw_input())
+item.append(float(raw_input())
